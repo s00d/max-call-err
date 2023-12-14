6d86d2246746fe40ced83b0cc612ef88b60d4dd9
Pavel Kuzmin <Virus191288@gmail.com>
2023-12-14 09:03:18 +0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,72 @@
+const isDev = process.env.NODE_ENV !== 'production';
+const currentDate = new Date();
+const timestamp = currentDate.getTime();
+
+export default defineNuxtConfig({
+  app: {
+    baseURL: '/',
+    buildAssetsDir: 'assets'
+  },
+  experimental: {
+    payloadExtraction: false
+  },
+  nitro: {
+    inlineDynamicImports: true
+  },
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    timestamp,
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      timestamp
+    }
+  },
+  vite: {
+    resolve: {
+      alias: {
+        'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
+      },
+    },
+  },
+  // debug: true,
+  modules: [
+    [
+      '@nuxtjs/i18n',
+      {
+        vueI18n: './i18n.config.ts',
+        locale: 'en',
+        defaultLocale: 'en',
+        lazy: true,
+        debug: false,
+        langDir: 'lang',
+        // compilation: {
+        //   jit: false
+        // },
+        bundle: {
+          runtimeOnly: false,
+        },
+        experimental: {
+
+        },
+        locales: [
+          {
+            code: 'en',
+            file: 'en.json'
+          },
+          {
+            code: 'ru',
+            file: 'ru.json'
+          },
+          {
+            code: 'de',
+            file: 'de.json'
+          }
+        ],
+      }
+    ],
+  ],
+
+  devtools: {
+    enabled: false,
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    test
+  </div>
+</template>
+<script setup lang="ts">
+import {useI18n} from "#imports";
+
+</script>
