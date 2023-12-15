f1647208c1c2ca0cb351ce87592177abcfdcb350
Pavel Kuzmin <Virus191288@gmail.com>
2023-12-15 13:16:28 +0500

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
-    test
+    <div v-text="$t('welcome')"></div>
   </div>
 </template>
 <script setup lang="ts">
-import {useI18n} from "#imports";
+import {useI18n, useHead, useNuxtApp} from "#imports";
+const { $i18n } = useNuxtApp();
+
+useHead({ title: $i18n.t("welcome") })
 
 </script>
